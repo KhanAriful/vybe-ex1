0514ce9e67c052c4aabdfdf8191b506cfe724df1
Ariful <ank4219@gmail.com>
2020-07-04 12:54:33 +0200

    init

---
+++ precache-manifest.84b2fd98fc52d525913c00b028157547.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05aa44d9993461fbcfcd710af2e3df87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39ee7105e46e080db2d",
+    "url": "/static/css/2.3cb7873e.chunk.css"
+  },
+  {
+    "revision": "ed1a1f2b0e88784ef4c7",
+    "url": "/static/css/main.5b2287a4.chunk.css"
+  },
+  {
+    "revision": "b39ee7105e46e080db2d",
+    "url": "/static/js/2.1df2cfc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1df2cfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1a1f2b0e88784ef4c7",
+    "url": "/static/js/main.c069e301.chunk.js"
+  },
+  {
+    "revision": "87236a43b0cd09d90945",
+    "url": "/static/js/runtime-main.e2b80782.js"
+  },
+  {
+    "revision": "6abf597a1f3f44fd10bdcb7b86b494f0",
+    "url": "/static/media/Nunito-Black.6abf597a.ttf"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "19caf1c06e022593ea4657734d691c86",
+    "url": "/static/media/Nunito-ExtraBold.19caf1c0.ttf"
+  },
+  {
+    "revision": "76ae20d4d4b7d46e613d2581512fcf73",
+    "url": "/static/media/Nunito-SemiBold.76ae20d4.ttf"
+  },
+  {
+    "revision": "7a9f18435edbcc3d10758778ca51bdb9",
+    "url": "/static/media/icomoon.7a9f1843.ttf"
+  }
+]);
